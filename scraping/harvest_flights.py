a967d21af296087eced00ce2c4430f1abc063a87
Josh Peckham <jsp589@usask.ca>
2022-09-13 15:05:02 -0600

    cleaned up code for harvest_flights.py -jp

--- scraping/harvest_flights.py
+++ scraping/harvest_flights.py
@@ -8,7 +8,6 @@ import os
 from os.path import exists
 
 
-
 def harvest_data(departure_location):
     url = "https://www.airports-worldwide.info/search/"+departure_location+"/departures"
     url = url.encode('ascii', errors='ignore')
@@ -19,20 +18,14 @@ def harvest_data(departure_location):
     for i in range(len(dfs)):
         datable_list.append(dfs[i])
 
-    #check if file is empty
     return pd.concat(datable_list)
-    # if not exists(file_output):
-    #     pd.concat(datable_list, ignore_index=True).to_csv(file_output)
-    # else:
-    #     pd.concat(datable_list, ignore_index=True).to_csv(file_output, mode='a', header=False)
 
-                
+
 if __name__ == "__main__":
-
-    #file out
+    # file out
     file_output="../__data/airport_departure.csv"
 
-    #get user location
+    # get user location
     user_location = "calgary"
     user_airport_timetable_data = harvest_data(user_location)
 
@@ -41,11 +34,11 @@ if __name__ == "__main__":
     separator = '('
     departures = user_airport_timetable_data['Destination'].unique().tolist()
 
+    for i in range(len(departures)):
+        departures[i] = departures[i].split(separator, 1)[0]
+        departures[i] = departures[i].rstrip()
+        departure = departures[i]
 
-
-    for i in departures:
-        departure = i.split(separator,1)[0]
-        departure = i.rstrip()
         try:
             ap_dep_df = harvest_data(departure)
 
@@ -55,6 +48,6 @@ if __name__ == "__main__":
                 ap_dep_df.to_csv(file_output, mode='a', header=False, index=False)
 
         except Exception as e:
-            print("skipping url do to an exception",e)
+            print(f"skipping url for {departure} do to an exception:",e)
     
 
