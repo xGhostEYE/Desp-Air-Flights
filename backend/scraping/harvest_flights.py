92f984bcdbf63b81b9d74b6b6684ff0fd91027ff
xGhostEYE <melvin.lugo@hotmail.com>
2022-10-11 00:23:45 -0600

    most data is cleaned

--- backend/scraping/harvest_flights.py
+++ backend/scraping/harvest_flights.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from asyncio.windows_events import NULL
 # from asyncio.windows_events import NULL
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -10,15 +11,13 @@ from os.path import exists
 
 
 
-# remove flights with no flight number
-# remove loops in path
-# remove cargo flight
+
 # format times
 
 
 
 def harvest_data_arrivals(arrival_location):
-    url = "https://www.airports-worldwide.info/airport/"+arrival_location+"/arrivals"
+    url = "https://www.airports-worldwide.info/search/"+arrival_location+"/arrivals"
     url = url.encode('ascii', errors='ignore')
     url = url.decode('ascii', errors='ignore')
     dfs = pd.read_html(url.replace(" ","%20"), header=0)
@@ -29,6 +28,14 @@ def harvest_data_arrivals(arrival_location):
 
     df = pd.concat(datable_list)
     df = df[df["Status"].isin(["scheduled", "scheduleddelayed"])]
+    #remove flights with no flight number
+    df = df.dropna(axis=0, subset=['Flight'])
+    #remove flight loop
+    discard = [arrival_location]
+    df = df[df["Origin"].str.contains('|'.join(discard))==False]
+    #remove cargo flights
+    # discard = ["cargo"]
+    # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
     return df
 
 
@@ -44,42 +51,66 @@ def harvest_data_departures(departure_location):
 
     df = pd.concat(datable_list)
     df = df[df["Status"].isin(["scheduled", "scheduleddelayed"])]
+    #remove flights with no flight number
+    df = df.dropna(axis=0, subset=['Flight'])
+    #remove flight loop
+    discard = [departure_location]
+    df = df[df["Destination"].str.contains('|'.join(discard))==False]
+    #remove cargo flights
+    # discard = ["cargo"]
+    # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
     return df
 
 #currently only removes the second time
 def clean_data(file):
-    df = pd.read_csv(file)
+    cleanedflights_flightnumber = []
+    cleanedflights_time = []
+    flights = []
+    df = pd.read_csv(file,encoding='utf8')
 
+    #remove second time
     for name, values in df[['Departure']].items():
         data = values.str.split()
         for i in range(len(data)):
-            # Clean the string
             cleaned_string = str(data[i]).strip("[]'")
-            
-            # Count the string, and only parse longer results which have the extra data we're looking for
-            chrcount = len(cleaned_string)
-            if chrcount>9:
-                print(cleaned_string[round(chrcount/2):])
-    for name, vlaues in df[['Flight']].items():
-        print(data)
+            cleanedflights_time.append(cleaned_string)
+    
+    #clean flight number
+    for item in df['Flight'].unique():
+        item = str(item).replace(" ", "")
+        if len(item) > 1:
+            if (str(item[0]).isalpha() and str(item[1]).isalpha()):
+                if (len(item) < 7):
+                    flights.append(str(item))
+                else:
+                    for i in range(len(item)):
+                        i+2  
+                        if (str(item[i]).isalpha() and str(item[i+1]).isalpha()):
+                            flights.append(str(item).rsplit(item[i],1)[0])
+                            continue
+    for i in range(len(flights)):
+        if flights[i] == '' or len(flights[i])>7:
+            continue
+        else:
+            cleanedflights_flightnumber.append(flights[i])
                 
 
 if __name__ == "__main__":
     # file out
-    file_output_origin_departures = os.path.abspath("origin_airport_departures.csv")
+    
 
     #forward scrape
     # get user location
     user_location = "calgary"
     user_airport_timetable_data = harvest_data_departures(user_location)
 
-    user_airport_timetable_data.to_csv(os.path.abspath("connecting_airport_departures.csv"), index=False)
+    user_airport_timetable_data.to_csv(os.path.abspath("origin_airport_departures.csv"), index=False)
 
     separator = '('
     departures = user_airport_timetable_data['Destination'].unique().tolist()
 
 
-
+    file_output_origin_departures = os.path.abspath("connecting_airport_departures.csv")
     for i in range(5):
         departures[i] = departures[i].split(separator, 1)[0]
         departures[i] = departures[i].rstrip()
@@ -98,13 +129,13 @@ if __name__ == "__main__":
     
     #backward scrape
     file_output_arrival=os.path.abspath("origin_airport_departures.csv")
-    user_location = "los angeles"
-    user_airport_timetable_data = harvest_data_departures(user_location)
+    user_requested_destination = "los angeles"
+    user_requested_airport_departures = harvest_data_arrivals(user_requested_destination)
 
-    user_airport_timetable_data.to_csv(os.path.abspath("user_arrival.csv"), index=False)
+    user_requested_airport_departures.to_csv(os.path.abspath("user_arrival.csv"), index=False)
 
     separator = '('
-    departures = user_airport_timetable_data['Origin'].unique().tolist()
+    departures = user_requested_airport_departures['Origin'].unique().tolist()
 
 
 
@@ -114,7 +145,7 @@ if __name__ == "__main__":
         departure = departures[i]
 
         try:
-            ap_dep_df = harvest_data_departures(departure)
+            ap_dep_df = harvest_data_arrivals(departure)
 
             if not exists(file_output_arrival):
                 ap_dep_df.to_csv(file_output_arrival, index=False)
@@ -124,4 +155,4 @@ if __name__ == "__main__":
             print(f"skipping url for {departure} do to an exception:",e)
     
     # clean the data for the Traveling salesman algo
-    clean_data(os.path.abspath("airport_destination.csv"))
+    clean_data(os.path.abspath(r"C:\Users\melvi\OneDrive\Usask\Year 4\Term 1\CMPT 370\comp370fall2022\connecting_airport_departures.csv"))
--- backend/scraping/test.py
+++ backend/scraping/test.py
@@ -1,5 +1,6 @@
 #currently only removes the second time
 from __future__ import print_function
+from cgi import print_exception
 # from asyncio.windows_events import NULL
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -12,18 +13,25 @@ from os.path import exists
 
 def clean_data(file):
     cleanedflights = []
-    df = pd.read_csv(file)
-    for name, values in df[['Flight']].items():
-        data = values.str.split()
-        for i in range(len(data)):
-            if data[i] is NaN or (str(data[i][0][0]).isalpha() == False or str(data[i][0][1]).isalpha() == False) :
-                continue
-            else:
-                print(data[i][0])
-                if len(str(data[i])) == 1:
-                    cleanedflights[i] = str(data[i][0])
-                if len(str(data[i])) >= 2:
-                    cleanedflights[i] = str.join(str(data[i][0]) + str(data[i][1]))
-                print(cleanedflights[i])
+    flights = []
+    df = pd.read_csv(file, encoding='utf8')
+    for item in df['Flight'].unique():
+        item = str(item).replace(" ", "")
+        if len(item) > 1:
+            if (str(item[0]).isalpha() and str(item[1]).isalpha()):
+                if (len(item) < 7):
+                    flights.append(str(item))
+                else:
+                    for i in range(len(item)):
+                        i+2  
+                        if (str(item[i]).isalpha() and str(item[i+1]).isalpha()):
+                            flights.append(str(item).rsplit(item[i],1)[0])
+                            continue
+    for i in range(len(flights)):
+        if flights[i] == '' or len(flights[i])>7:
+            continue
+        else:
+            cleanedflights.append(flights[i])
 
-clean_data("/student/mel196/Documents/comp370/comp370fall2022/connecting_airport_departures.csv")
+    print(cleanedflights)
+clean_data(r"C:\Users\melvi\OneDrive\Usask\Year 4\Term 1\CMPT 370\comp370fall2022\connecting_airport_departures.csv")
