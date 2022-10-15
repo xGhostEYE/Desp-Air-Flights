b15d37e5a9962a49c7befec32ee4326e34bf52fd
xGhostEYE <melvin.lugo@hotmail.com>
2022-10-14 22:31:59 -0600

    ignore this too

--- backend/scraping/test.py
+++ backend/scraping/test.py
@@ -18,45 +18,64 @@ def clean_data(df):
     Returns:
         lists of cleaned data
     """
+    print("entering clean data", df)
+    # for index,item in df['Flight'].items():
+    #     item = str(item).replace(" ", "")
+    #     if len(item) > 1:
+    #         if (str(item[0]).isalpha() and str(item[1]).isalpha() and str(item[2]).isalpha()==False):
+    #             continue
+    #         else:
+    #             df.drop(index,inplace=True)
     cleanedflights_flightnumber = []
     cleanedflights_time = []
     flights = []
+    #clean flight number
+    for index,item in df['Flight'].items():
+        item = str(item).replace(" ", "")
+        if len(item) > 1:
+            flights.append(str(item)[:6])
+        else:
+            df.drop(index,inplace=True)
+            # if (str(item[0]).isalpha() and str(item[1]).isalpha()):
+            #     if (len(item) <= 6):
+            #         flights.append(str(item))
+            #     else:
+            #         # flights.append(str(item)[:len(str(item))-6])
+            #         # changed here
+            #         for i in range(len(item)):
+            #             i+2  
+            #             if (str(item[i]).isalpha() and str(item[i+1]).isalpha()):
+            #                 flights.append(str(item).rsplit(item[i],1)[0])
+            #                 continue
+    for i in range(len(flights)):
+        if flights[i] == '' or len(flights[i])>6:
+            continue
+        elif str(flights[i][:-1]).isalpha():
+            flights[i] = str(flights[i])[:-1]
+            cleanedflights_flightnumber.append(flights[i])
+    
     departure_or_origin = ""
     if ('Departure' in df):
         departure_or_origin = 'Departure'
     else:
         departure_or_origin = 'Origin'
-        #remove second time
-    for name, values in df[[departure_or_origin]].items():
-        data = values.str.split()
-        for i in range(len(data)):
-            cleaned_string = str(data[i]).strip("[]'")
-            cleanedflights_time.append(cleaned_string)
-                    
-    #clean flight number
-    for item in df['Flight'].unique():
-        item = str(item).replace(" ", "")
-        if len(item) > 1:
-            if (str(item[0]).isalpha() and str(item[1]).isalpha()):
-                if (len(item) < 7):
-                    flights.append(str(item))
-                else:
-                    for i in range(len(item)):
-                        i+2  
-                        if (str(item[i]).isalpha() and str(item[i+1]).isalpha()):
-                            flights.append(str(item).rsplit(item[i],1)[0])
-                            continue
-    for i in range(len(flights)):
-        if flights[i] == '' or len(flights[i])>7:
-            continue
+    print("after clean flights number",df)
+    #clean time
+    time_list = list(df[departure_or_origin])
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i])
+        if (len(time_list[i])>5):
+            time_list[i] = time_list[i][-5:]
+            cleanedflights_time.append(time_list[i])
         else:
-            cleanedflights_flightnumber.append(flights[i])
-    
+            cleanedflights_time.append(time_list[i])
+    print("after clean time number",df)
     df_cleaned = pd.DataFrame (cleanedflights_flightnumber, columns = ['Flight'])
     df_cleaned[departure_or_origin] = cleanedflights_time
     df[departure_or_origin] = df_cleaned[departure_or_origin].values
     df['Flight'] = df_cleaned['Flight'].values
     return (df)
+
 
 
 def harvest_data_arrivals(arrival_location):
@@ -69,15 +88,14 @@ def harvest_data_arrivals(arrival_location):
         dataframe with arriving flights to airport
 
     """
-    "*** error with url, airport code may work ***"
-    url = "https://www.airports-worldwide.info/airport/"+arrival_location+"/arrivals"
+    url = "https://www.airports-worldwide.info/search/"+arrival_location+"/arrivals"
     url = url.encode('ascii', errors='ignore')
     url = url.decode('ascii', errors='ignore')
     dfs = pd.read_html(url.replace(" ","%20"), header=0)
     
     # combines dataframes
     df = pd.concat(dfs)
-
+    print(df)
     # removes all flights that do not contain "scheduled" in "Status" column
     df = df[df["Status"].str.contains('scheduled', regex=False)]
 
@@ -91,8 +109,9 @@ def harvest_data_arrivals(arrival_location):
     #remove cargo flights
     # discard = ["cargo"]
     # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
-    
-    return clean_data(df)
+    print("harvest arrival calling\n")
+    data = clean_data(df)
+    return data
 
 
 
@@ -123,12 +142,13 @@ def harvest_data_departures(departure_location):
 
     #remove flight loop
     discard = [departure_location]
-    df = df[df["Departure"].str.contains('|'.join(discard))==False]
+    df = df[df["Destination"].str.contains('|'.join(discard))==False]
     #remove cargo flights
     # discard = ["cargo"]
     # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
-
-    return clean_data(df)
+    print("harvest departure calling\n")
+    data = clean_data(df)
+    return data
 
 #currently only removes the second time
 
@@ -149,7 +169,7 @@ if __name__ == "__main__":
     
     file_output_origin_departures = os.path.abspath("connections_to_destination.csv")
 
-    for i in range(5):
+    for i in range(len(departures)):
         departures[i] = departures[i].split(separator, 1)[0]
         departures[i] = departures[i].rstrip()
         departure = departures[i]
@@ -166,18 +186,21 @@ if __name__ == "__main__":
     #backward scrape
     
     user_requested_destination = "los angeles"
+    print("at harvest arrivals")
     user_requested_airport_departures = harvest_data_arrivals(user_requested_destination)
-    user_requested_airport_departures.to_csv(os.path.abspath("user_arrival.csv"), index=False)
+    
+    user_requested_airport_departures.to_csv(os.path.abspath("destination_airport_arrivals.csv"), index=False)
     separator = '('
     departures = user_requested_airport_departures['Origin'].unique().tolist()
-    
+    print("before connections from destinations")
     file_output_arrival=os.path.abspath("connections_from_destination.csv")
-    for i in range(5):
+    print("after connections from destination")
+    for i in range(len(departures)):
         departures[i] = departures[i].split(separator, 1)[0]
         departures[i] = departures[i].rstrip()
         departure = departures[i]
         try:
-            ap_dep_df = harvest_data_arrivals(departure)
+            ap_dep_df = harvest_data_departures(departure)
             if not exists(file_output_arrival):
                 ap_dep_df.to_csv(file_output_arrival, index=False)
             else:
