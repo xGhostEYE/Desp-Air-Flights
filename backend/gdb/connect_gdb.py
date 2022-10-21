942213a410e12b3242834206f7763660c5bd850d
Josh Peckham <jsp589@usask.ca>
2022-10-21 14:45:44 -0600

    got py2neo to connect to docker gdb -jp

--- backend/gdb/connect_gdb.py
+++ backend/gdb/connect_gdb.py
@@ -1,10 +1,8 @@
-import pandas as pd
 from py2neo import Graph
-import numpy as np
 
-# gdb = Graph(scheme="http", host="cmpt370-db", port="7474", user="neo4j", password="password", name="neo4j", secure=True)
-gdb = Graph("bolt://localhost:7687", user="neo4j", password="password")
+def connect_gdb():
+    gdb = Graph("bolt://cmpt370-db:7687", user="neo4j", password="password")
+    return gdb
 
-gdb.run("MERGE (:Test {testNum:1})")
-print(gdb)   
+   
 
