0a436e27cd56127c23ee0015f72a88d5a9855999
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2022-10-26 21:52:12 -0600

    added search button and text area for home page

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Big fat butts!" />
+      <HomePage msg="Big" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -9,12 +9,15 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <p>Starting</p>
+    <input
+      :value="starting"
+      @input="event => starting = event.target.value">
+      <p>Destination</p>
+    <input
+      :value="destination"
+      @input="event => destination = event.target.value">
+      <button>Search</button>
   </div>
 </template>
 
