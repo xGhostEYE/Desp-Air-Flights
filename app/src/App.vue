3857da2704076634ee2080679138f94f764990af
Riley Chometa <RPC619@USASK.CA>
2022-11-01 19:58:44 -0600

    trying to fix issue with page not loading

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,61 +1,15 @@
 <script>
+import Flights from './components/FlightPage.vue';
 import HomePage from './components/HomePage.vue';
-
-export default{
-    data() {
-        return {
-            title: "Desp-Air Flights"
-        };
-    },
-    components: { HomePage }
-}
-</script>
-
-
-
-
-<template>
-  
-  <div class="title display-1 fixed-top">
-    <h1 class="text-center">{{title}}</h1>
-  </div>
-  <div class="search_box">
-    <p class="mb-0">Starting</p>
-    <input
-      :value="starting"
-      @input="event => starting = event.target.value">
-      <p class = "mt-5 mb-0">Destination</p>
-    <input
-      :value="destination"
-      @input="event => destination = event.target.value">
-      <div>
-        <button class="mt-5 btn btn-info">Search</button>
-      </div>
-  </div> 
-
- 
-</template>
-
-<template>
-  <div id="flights">
-    <button @click="showFlights">Search</button>
-    <first-component
-        v-if="showComponentOne"
-    />
-  </div>
-</template>
-
-
-<script>
-import Flghts from './components/FlightPage'
-
 export default {
   name: "App",
   components: {
-    Flights
+    Flights,
+    HomePage
   },
   data() {
     return {
+      title: "Desp-Air Flights",
       myFlight: false
     };
   },
@@ -66,6 +20,37 @@ export default {
   }
 };
 </script>
+
+<template>
+  <div>
+    <div class="title display-1 fixed-top">
+      <h1 class="text-center">{{title}}</h1>
+    </div>
+    <div class="search_box">
+      <p class="mb-0">Starting</p>
+      <input
+        :value="starting"
+        @input="event => starting = event.target.value">
+        <p class = "mt-5 mb-0">Destination</p>
+      <input
+        :value="destination"
+        @input="event => destination = event.target.value">
+        <div>
+          <button class="mt-5 btn btn-info">Search</button>
+        </div>
+    </div> 
+  </div>
+  <!-- <div id="flights">
+    <button @click="showFlights">Search</button>
+    <first-component
+        v-if="myFlight"
+    />
+  </div> -->
+ 
+</template>
+
+
+
 
 
 
