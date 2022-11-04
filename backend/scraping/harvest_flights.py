c30d6c0d23dbff0da13b5750f44992f2264d9183
Melvin Lugo <melvin.lugo@hotmail.com>
2022-11-03 23:36:27 -0600

    harvest_flights updated, all time harvest workin

--- backend/scraping/harvest_flights.py
+++ backend/scraping/harvest_flights.py
@@ -10,6 +10,8 @@ import pandas as pd
 from pandas import *
 import os
 from os.path import exists
+import requests
+import re
 
 
 # format times
@@ -22,13 +24,6 @@ def clean_data(df):
     Returns:
         lists of cleaned data
     """
-    # for index,item in df['Flight'].items():
-    #     item = str(item).replace(" ", "")
-    #     if len(item) > 1:
-    #         if (str(item[0]).isalpha() and str(item[1]).isalpha() and str(item[2]).isalpha()==False):
-    #             continue
-    #         else:
-    #             df.drop(index,inplace=True)
     cleanedflights_flightnumber = []
     cleanedflights_time = []
     flights = []
@@ -39,17 +34,6 @@ def clean_data(df):
             flights.append(str(item)[:6])
         else:
             df.drop(index,inplace=True)
-            # if (str(item[0]).isalpha() and str(item[1]).isalpha()):
-            #     if (len(item) <= 6):
-            #         flights.append(str(item))
-            #     else:
-            #         # flights.append(str(item)[:len(str(item))-6])
-            #         # changed here
-            #         for i in range(len(item)):
-            #             i+2  
-            #             if (str(item[i]).isalpha() and str(item[i+1]).isalpha()):
-            #                 flights.append(str(item).rsplit(item[i],1)[0])
-            #                 continue
     for i in range(len(flights)):
         if flights[i] == '':
             continue
@@ -59,13 +43,18 @@ def clean_data(df):
         else:
             cleanedflights_flightnumber.append(flights[i])
     
-    departure_or_origin = ""
+    departure_or_arrival = ""
     if ('Departure' in df):
-        departure_or_origin = 'Departure'
+        departure_or_arrival = 'Departure'
     else:
-        departure_or_origin = 'Origin'
+        departure_or_arrival = 'Arrival'
+    destination_or_origin = ""
+    if ('Destination' in df):
+        destination_or_origin = 'Destination'
+    else:
+        destination_or_origin = 'Origin'
     #clean time
-    time_list = list(df[departure_or_origin])
+    time_list = list(df[departure_or_arrival])
     for i in range(len(time_list)):
         time_list[i] = str(time_list[i])
         if (len(time_list[i])>5):
@@ -74,47 +63,18 @@ def clean_data(df):
         else:
             cleanedflights_time.append(time_list[i])
     df_cleaned = pd.DataFrame (cleanedflights_flightnumber, columns = ['Flight'])
-    df_cleaned[departure_or_origin] = cleanedflights_time
-    df[departure_or_origin] = df_cleaned[departure_or_origin].values
+    df_cleaned[departure_or_arrival] = cleanedflights_time
+    df[departure_or_arrival] = df_cleaned[departure_or_arrival].values
     df['Flight'] = df_cleaned['Flight'].values
+    # seperating airport code and city name
+    new = df[destination_or_origin].str.split("(", n = 1, expand = True)
+    df["City Name"]= new[0]
+    df["Airport Code"] = new[1]
+    df["Airport Code"] = df["Airport Code"].str.replace(r')', '')
+    df.drop(columns =[destination_or_origin], axis=1,inplace = True)
+    # df.drop(df.columns.difference(['a','b']), 1, inplace=True)
     return (df)
 
-
-
-def harvest_data_departures(departure_location):
-    """
-    Purpose:
-        scrapes flights departing from given airport
-    Params:
-        departure_location: airport we are scraping
-    Returns:
-        dataframe with departing flights from airport
-    
-    """
-    url = "https://www.airports-worldwide.info/search/"+departure_location+"/departures"
-
-    url = url.encode('ascii', errors='ignore')
-    url = url.decode('ascii', errors='ignore')
-    dfs = pd.read_html(url.replace(" ","%20"), header=0)
-    
-    # combines dataframes
-    df = pd.concat(dfs)
-
-    # removes all flights that do not contain "scheduled" in "Status" column
-    df = df[df["Status"].str.contains('scheduled', regex=False)]
-
-    #remove flights with no flight number
-    df = df.dropna(axis=0, subset=['Flight'])
-
-    #remove flight loop
-    # discard = [arrival_location]
-    # df = df[df["Origin"].str.contains('|'.join(discard))==False]
-
-    #remove cargo flights
-    # discard = ["cargo"]
-    # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
-
-    return df
 
 
 def harvest_data_arrivals(arrival_location):
@@ -128,41 +88,128 @@ def harvest_data_arrivals(arrival_location):
 
     """
     url = "https://www.airports-worldwide.info/search/"+arrival_location+"/arrivals"
-    url = url.encode('ascii', errors='ignore')
-    url = url.decode('ascii', errors='ignore')
-    dfs = pd.read_html(url.replace(" ","%20"), header=0)
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    urls = []
+    list_of_dataframes = []
+    interval = soup.find_all("nav", {"id": "intervals"})
+    interval = soup.find_all("nav", {"id": "intervals"})
+    if (len(interval)<=0):
+        url = url.encode('ascii', errors='ignore')
+        url = url.decode('ascii', errors='ignore')
+        dataframes = pd.read_html(url.replace(" ","%20"))
+        for i in range(len(dataframes)):
+            list_of_dataframes.append(dataframes[i])
+    else:
+        data = re.findall(r'(https?://[^\s]+)', str(interval[0]))
+        for i in range(len(data)):
+            urls.append(data[i].split(">", 1)[0])
+
+        for i in range(len(urls)):
+            
+            urls[i] = urls[i].encode('ascii', errors='ignore')
+            urls[i] = urls[i].decode('ascii', errors='ignore')
+            dataframes = pd.read_html(urls[i].replace(" ","%20"))
+            for i in range(len(dataframes)):
+                list_of_dataframes.append(dataframes[i])
     
     # combines dataframes
-    df = pd.concat(dfs)
+    df = pd.concat(list_of_dataframes)
     
     # removes all flights that do not contain "scheduled" in "Status" column
     df = df[df["Status"].str.contains('scheduled', regex=False)]
-
+    
+    #remove unammed columns
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    
     #remove flights with no flight number
     df = df.dropna(axis=0, subset=['Flight'])
 
     # remove flight loop
     discard = [arrival_location]
     df = df[df["Origin"].str.contains('|'.join(discard))==False]
-
+    
     #remove cargo flights
     # discard = ["cargo"]
     # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
     data = clean_data(df)
     return data
 
+
+def harvest_data_departures(departure_location,initial_search):
+    """
+    Purpose:
+        scrapes flights departing from given airport
+    Params:
+        departure_location: airport we are scraping
+    Returns:
+        dataframe with departing flights from airport
+    
+    """
+
+    url = "https://www.airports-worldwide.info/search/"+departure_location+"/departures"
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    urls = []
+    list_of_dataframes = []
+    interval = soup.find_all("nav", {"id": "intervals"})
+    interval = soup.find_all("nav", {"id": "intervals"})
+    if (len(interval)<=0):
+        url = url.encode('ascii', errors='ignore')
+        url = url.decode('ascii', errors='ignore')
+        dataframes = pd.read_html(url.replace(" ","%20"))
+        for i in range(len(dataframes)):
+            list_of_dataframes.append(dataframes[i])
+    else:
+        data = re.findall(r'(https?://[^\s]+)', str(interval[0]))
+        for i in range(len(data)):
+            urls.append(data[i].split(">", 1)[0])
+
+        for i in range(len(urls)):
+            
+            urls[i] = urls[i].encode('ascii', errors='ignore')
+            urls[i] = urls[i].decode('ascii', errors='ignore')
+            dataframes = pd.read_html(urls[i].replace(" ","%20"))
+            for i in range(len(dataframes)):
+                list_of_dataframes.append(dataframes[i])
+    
+    # combines dataframes
+    df = pd.concat(list_of_dataframes)
+
+    # removes all flights that do not contain "scheduled" in "Status" column
+    df = df[df["Status"].str.contains('scheduled', regex=False)]
+
+    #remove flights with no flight number
+    df = df.dropna(axis=0, subset=['Flight'])
+    
+    #remove unammed columns
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    
+    #remove flight loop
+    discard = [departure_location]
+    df = df[df["Destination"].str.contains('|'.join(discard))==False]
+    #remove cargo flights
+    # discard = ["cargo"]
+    # df = df[df["Carrier"].str.contains('|'.join(discard))==False]
+    data = clean_data(df)
+    new_row = {"City Name":"above data from: "+departure_location}
+    #append row to the dataframe
+    data = data.append(new_row, ignore_index=True)
+    return data
+
+
 if __name__ == "__main__":
     
     # scrape departures from airport
     # airport flights depart from
     departure_airport = "calgary"
-
+    initial_search = True
     # file out
     departures_file_out = f"./__data/{departure_airport}_airport_departures.csv"
 
     # scrape airport departures
-    airport_dept_df = harvest_data_departures(departure_airport)
-
+    airport_dept_df = harvest_data_departures(departure_airport, initial_search)
+    initial_search = False
     # save airport departures to csv
     airport_dept_df.to_csv(departures_file_out, index=False)
 
