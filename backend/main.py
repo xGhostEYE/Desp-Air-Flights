fc77097307e20ac45fbf860d5687460472ddd17c
Riley Chometa <RPC619@USASK.CA>
2022-11-05 15:14:40 -0600

    testing out backend api and how it will work

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
 from flask import Flask
 
 backend = Flask(__name__)
-@backend.route("/")
+
+# test data:
+airports = [
+    { 
+      'city': 'Saskatoon', 
+      'code': 'YXE' 
+    },
+    { 
+      'city': 'Regina',
+      'code': 'YQR'
+    }
+]
+
 def main():
   return "It Works!"
+
 if __name__ == '__main__':
   backend.run(debug=True)
+
+@backend.route("/airports")
+def get_airports():
+  return jsonify(airports)
