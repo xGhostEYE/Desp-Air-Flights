efffe596731131fad22d9810ae4f0a64b448a287
Josh Peckham <jsp589@usask.ca>
2022-11-15 12:21:15 -0600

    bug fixing script -jp

--- backend/search/find_path.py
+++ backend/search/find_path.py
@@ -114,26 +114,25 @@ def get_paths(departure, destination, number_of_paths=1):
         if paths_df is None:
             return None
 
-        
-        
-        # path_df is the dataframe with data on curPath
-        pathCheck_df = paths_df[paths_df["path"] >= num_paths_already_checked]
+        # dataframe of paths that havent been checked
+        pathCheck_df = paths_df[paths_df["path"] > num_paths_already_checked]
 
+        # iterates through and checks paths for validity, if valid adds to validPaths_df
         paths = pathCheck_df["path"].unique().tolist()
-
         for path in paths:
             path_df = pathCheck_df[pathCheck_df["path"] == path]
             if path_is_valid(path_df):
                 validPaths_df = pd.concat([validPaths_df, path_df])
                 number_of_valid_paths +=1
-            if number_of_valid_paths == number_of_paths:
-                break
 
+                if number_of_valid_paths == number_of_paths:
+                    break
+        
         num_paths_already_checked = num_paths_to_query
         num_paths_to_query = num_paths_to_query * 2
 
-        # checks if curPath is greater than maxPath, meaning there are no more paths
-        # to check and breaks loop
+        # checks if num_paths_already_checked is greater than maxPath, meaning
+        # there are no more paths to check and breaks loop
         maxPath = paths_df["path"].max()
         if maxPath < num_paths_already_checked:
             break
@@ -201,16 +200,16 @@ def get_paths_json(departure, destination, number_of_paths=1):
 if __name__ == "__main__":
     
     departure = "Richmond"
-    destination = "Mississauga"
+    destination = "Calgary"
 
     # paths = get_paths_from_dijkstra(departure, destination, 10000)
     # paths.to_csv(f"./__data/test_paths/{departure}_to_{destination}_test.csv", index=False)
 
-    # paths = get_paths(departure, destination, 10)
-    # paths.to_csv(f"./__data/test_paths/{departure}_to_{destination}.csv", index=False)
+    paths = get_paths(departure, destination, 10)
+    paths.to_csv(f"./__data/test_paths/{departure}_to_{destination}.csv", index=False)
 
     
-    paths = get_paths_json(departure, destination, 10)
-    print(paths)
+    # paths = get_paths_json(departure, destination, 10)
+    # print(paths)
 
     
