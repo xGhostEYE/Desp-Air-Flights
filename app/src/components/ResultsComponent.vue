cee05444321098e88ad6c65b14c49bd20431b984
Kim <ykm535@usask.ca>
2022-11-19 21:20:24 -0600

    collapsing/expanding flight info working

--- app/src/components/ResultsComponent.vue
+++ app/src/components/ResultsComponent.vue
@@ -8,7 +8,9 @@ defineProps({
 </script>
 
 
+
 <template>
+  <!--
   <div class="column" style="height:10px; width: 1000px;">
     <li v-for="resultObject in this.results">
       <div class="card w-75">
@@ -31,7 +33,33 @@ defineProps({
         </div>  
       </div>
     </li>
-  </div>  
+  </div>-->
+
+<div class="column" style="height:10px; width: 500px;">
+  <div class="accordion" v-if="this.results != null" id="accordionPanelsStayOpenExample">
+    <li v-for="resultObject in this.results">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+          <button class="accordion-button" type="button" @click="resultObject.isVisible = !resultObject.isVisible" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            {{ resultObject.startTime }} - {{ resultObject.endTime }}    {{ resultObject.totalTime }}    {{ resultObject.totalCost }}
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseOne" :class="resultObject.isVisible?'show': ''" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+          <div class="accordion-body">
+          <span v-for="item in resultObject.flights">
+              <p class="card-text">{{ item.departure.time }}   -    {{ item.departure.location }} ({{ item.departure['airport code'] }})</p>
+              <BIconArrowDown/>
+              <p class="card-text">{{ item.arrival.time }}   -    {{ item.arrival.location }} ({{ item.arrival['airport code'] }})</p>
+              <p class="card-text">{{ item.airline }}</p>
+              <button @click="redirectToFlight(resultObject.url)" class="mt-5 btn btn-primary" style="background-color: #3E6D9C;">Take me to this flight!</button>
+              <hr/>
+          </span>
+          </div>
+        </div>  
+      </div>
+    </li> 
+  </div>
+</div>
 </template>
 
 
@@ -40,15 +68,15 @@ defineProps({
 <script>
 export default {
   name: "Results",
-  methods: {
-      redirectToFlight: function(url){
-        window.open(url)
-      }
   /* data() {
     return {
       
     };
-  } */
+  },*/
+  methods: {
+      redirectToFlight: function(url){
+        window.open(url)
+      } 
   }
 };
 </script>
--- app/src/main.js
+++ app/src/main.js
@@ -8,9 +8,9 @@ import router from './router'
 
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import { BIconArrowDown} from 'bootstrap-icons-vue';
 const app = createApp(App);
-
+app.component('BIconArrowDown', BIconArrowDown);
 
 
 app.use(createPinia());
